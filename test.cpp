5dcc8dc20c55f8f8747f5b4ec1926df01d995ce8
hikariko-aoe <amod091@protonmail.com>
2020-10-08 23:39:00 -0500

    edit commind test

--- test.cpp
+++ test.cpp
@@ -3,7 +3,8 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello World!, test no2, nvm no3, good!!!";
+    std::cout << "Hello World!, test no2, nvm no3, good,commit test, nvm just a edit commit test!!!";
     return 0;
+
     system("pause");
 }
