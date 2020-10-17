cb27d2ae31a3d25ca0406945c6548ba7c20111d1
hikariko-aoe <amod091@protonmail.com>
2020-10-17 13:36:12 -0500

    Final

--- Basico-Problemas_Semanales/Bellos_Muffins__OmegaUp.cpp
+++ Basico-Problemas_Semanales/Bellos_Muffins__OmegaUp.cpp
@@ -4,17 +4,20 @@
 using namespace std;
 
 int main(){
+    //Variables
     int x=0;
     int y=0;
     int div=0;
     int res=0;
-
+    //Pregunta No. De Muffins
     cout<<"No. de Muffins que tienes para compartir:"<<endl;
     cin>>x;
     cout<<"No. de compas que van a comer:"<<endl;
     cin>>y;
+    //Operaciones correspondientes
     div=x/y;
     res=x%y;
+    //impresion del resultado
     cout<<"Tu y tus compas se van a comer ["<<div<<"] muffins !"<<endl;
     cout<<"Entonces tu compa Gil se va a comer ["<<div+res<<"] muffins !"<<endl;
 
