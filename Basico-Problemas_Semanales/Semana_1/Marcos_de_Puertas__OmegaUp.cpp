541c35c7646aa76efa8be5d476205a99eafd9488
hikariko-aoe <amod091@protonmail.com>
2020-10-27 21:28:55 -0600

    [Placeholder No.2]

--- Basico-Problemas_Semanales/Semana_1/Marcos_de_Puertas__OmegaUp.cpp
+++ Basico-Problemas_Semanales/Semana_1/Marcos_de_Puertas__OmegaUp.cpp
@@ -1,8 +1,21 @@
-#include <iostream>
+#include <iostream>         //uwu/♫♫♫ https://www.youtube.com/watch?v=CI7kgaY9Rso&list=PLk6H3m3lpS_lSjT2dsKEhV4eyCWJpSqtA
 
 using namespace std;
 
 int main(){
+    //Variables
+    int a, b, n, x, ab;
+    //Long, de las barras
+    cin>>n;
+    //Long, Lados verticales (a*2)
+    cin>>a;
+    a=a*2;
+    //Long, Lado superior
+    cin>>b;
+    //Operaciones/Calculos
+    
+    //Impresion
+    cout<<x;
 
-    int a, b, n;
+    return 0;
 }
