8875ab0e4f750473bef17d6b580fdf3156ed1bc8
hikariko-aoe <amod091@protonmail.com>
2020-10-28 18:37:16 -0600

    Finished-Fixed !

--- Basico-Problemas_Semanales/Semana_1/2018_1_CONDICIONES3__OmegaUp.cpp
+++ Basico-Problemas_Semanales/Semana_1/2018_1_CONDICIONES3__OmegaUp.cpp
@@ -6,6 +6,7 @@ int main(){
     //Variables float para admitir decimales
     float sal;
     float imp;
+    float exceso;
     //Introducir tu salario
     cin>>sal;
     //Si el salario es menor a 969.5, sacarle el 1.92% de impuestos.
@@ -15,28 +16,34 @@ int main(){
     }
     //Si el el salario es mayor a 969 y menor a 1703, agregar el impuesto fijo, calcular el dinero sobresaliente y el porcentaje adicional
     else if (sal>=969.51 && sal<=1703.80){
-        imp=56.91+((sal-969.51)/100*10.88);
+        exceso=sal-969.51;
+        imp=56.91+(exceso/100*10.88);
         cout<<imp;
     }
     //igual que el else if anterior
     else if (sal>=1703.81 && sal<=4782.61){
-        imp=136.85+((sal-1703.81)/100*16);
+        exceso=sal-1703.81;
+        imp=136.85+(exceso/100*16);
         cout<<imp;
     }
     else if (sal>=4782.62 && sal<=14391.44){
-        imp=766.15+((sal-4762.62)/100*23.52);
+        exceso=sal-4782.62;
+        imp=766.15+(exceso/100*23.52);
         cout<<imp;
     }
     else if (sal>=14391.45 && sal<=19188.61){
-        imp=3470.25+((sal-14391.45)/100*32);
+        exceso=sal-14391.45;
+        imp=3470.25+(exceso/100*32);
         cout<<imp;
     }
     else if (sal>=19188.62 && sal<=57565.76){
-        imp=5005.35+((sal-19188.62)/100*34);
+        exceso=sal-19188.62;
+        imp=5005.35+(exceso/100*34);
         cout<<imp;
     }
     else if (sal>=57565.77){
-        imp=18053.63+((sal-57565.77)/100*35);
+        exceso=sal-57565.77;
+        imp=18053.63+(exceso/100*35);
         cout<<imp;
     }
 
