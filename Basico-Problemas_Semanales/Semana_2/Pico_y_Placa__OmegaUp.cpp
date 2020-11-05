0ac52502e4509010b11721f662ffa4a94e20aa18
hikariko-aoe <amod091@protonmail.com>
2020-11-04 21:49:57 -0600

    WIP

---
+++ Basico-Problemas_Semanales/Semana_2/Pico_y_Placa__OmegaUp.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int nPlaca;
+    int nDias;
+    int dInicio;
+
+    cin>>nPlaca;
+
+    if (nPlaca % 2 == 0){
+        cin>>nDias;
+        cin>>dInicio;
+        
+    }
+    else{
+        cin>>nDias;
+        cin>>dInicio;
+
+    }
+}
